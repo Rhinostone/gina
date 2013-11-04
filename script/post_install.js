60c6d63ff09751a262ac73d06936756295c18b5a
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2013-11-04 11:48:38 +0100

    chenged default bundles folder

--- script/post_install.js
+++ script/post_install.js
@@ -5,30 +5,51 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+var PostInstall;
 
-var Fs = require("fs"),
-    Utils = require("geena.utils"),
-    PostInstall = {
-    path : __dirname.substring(0, (__dirname.length - "script".length)),
+var Fs = require("fs"), EventEmitter = require('events').EventEmitter;
+var Utils = require("geena.utils");
+
+/**
+ * Post install constructor
+ * @constructor
+ * */
+PostInstall = function(){
+
+    var _this = this;
+    this.path = __dirname.substring(0, (__dirname.length - "script".length));
+
     /**
      * Initialize post installation scripts
      * */
-    init : function(){
-        this.createGeenaFile();
-    },
+    var init = function(){
+
+        createGeenaFile();
+            //.onComplete();
+    };
+
     /**
      * Creating framework command line file
      * */
-    createGeenaFile : function(){
+    var createGeenaFile = function(){
 
-        var name = require(this.path + 'package.json').name,
-            appPath = this.path.substring(0, (this.path.length - ("node_modules/" + name + '/').length)),
-            source = this.path + 'core/template/command/geena.tpl',
+        var name = require(_this.path + 'package.json').name,
+            appPath = _this.path.substring(0, (_this.path.length - ("node_modules/" + name + '/').length)),
+            source = _this.path + 'core/template/command/geena.tpl',
             target = appPath + name;
 
         Utils.Generator.createFileFromTemplate(source, target);
-    }
+    };
+
+    var createGeenaHome = function(){
+
+    };
+
+    init();
 };
-PostInstall.init();
+Util.inherits(PostInstall, EventEmitter);
+
+var postInstall = new PostInstall();
+//PostInstall.init();
 //module.exports = PostInstall;
 
