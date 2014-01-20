d7429ec9a8825e7877618871e39e59e6e527419f
Martin-Luther ETOUMAN <m.etouman@rhinostone.com>
2014-01-20 14:00:53 +0100

    Added support for custom connectors & debug for geena child processes

--- core/model/index.js
+++ core/model/index.js
@@ -25,7 +25,7 @@ var fs      = require('fs'),
     //UtilsConfig = Utils.Config(),
     //Dev     = Utils.Dev,
     util    = require('util'),
-    config  = require('./../config')(),
+    config  = require('./../config')(),//getContext()...
     EventEmitter  = require('events').EventEmitter;
 
 /**
@@ -35,6 +35,9 @@ var fs      = require('fs'),
 Model = function(namespace){
     var _this = this;
     var _configuration = null;
+    var _connector = null;
+    var _locals = null;
+    var utilsConfig = getContext('geena.utils.config');
 
     /**
      * Init
@@ -44,13 +47,13 @@ Model = function(namespace){
      * @private
      * */
     var init = function(namespace){
-        //TODO - if intance...
+        //TODO - if instance...
 
 
         if ( typeof(namespace) == "undefined" || namespace == "") {
             log.error('geena', 'MODEL:ERR:1', 'EEMPTY: Model namespace', __stack);
         }
-        var suffix = 'Entity';
+
         var namespace = namespace.split(/\//g);
         var bundle = namespace[0];
         namespace.shift();
@@ -66,103 +69,67 @@ Model = function(namespace){
             namespace[0] = namespace[0].substring(0, 1).toUpperCase() + namespace[0].substring(1);
             var model = namespace[0];
         }
+        _connector = model.toLowerCase();
 
         console.log("\nBundle", bundle);
         console.log("Model", model);
 
 
-        getConfig(bundle, function(err, conf){
+        getConfig(bundle, function onGetConfigDone(err, conf){
 
             if (!err) {
                 _configuration = conf.model;
                 console.log("CONF READY ", model, conf.path);
-                //TODO - More tries & catches...
-                //Getting Entities Manager.
-                var entitiesManager = new require(conf.path)()[model];
+                //TODO - More controls...
 
                 //For now, I just need the F..ing entity name.
-                var modelPath = conf.path + '/' + modelDirName
-                //console.log('modelPath: ', modelPath);
-                fs.readdir(modelPath, function(err, files){
+                var modelPath       = _(conf.path + '/' + modelDirName);
+                var entitiesPath    = _(modelPath + '/entities');
+                var connectorPath   = _(modelPath + '/lib/connector.js');
 
-                    var entityName, exluded = ['index.js'];
+                //Getting Entities Manager.
+                //var entitiesManager = new require(conf.path)()[model];
 
-                    var produce = function(entityName, i){
-                        //console.log("producing ", files[i]);
-                        //var utilsConfig = new utils.Config();
-                        var utilsConfig = getContext('geena.utils.config');
-                        utilsConfig.get('geena', 'locals.json', function(err, configuration){
-                            if (configuration == undefined) {
-                                throw new Error("geena/utils/.gna not found.");
+                fs.exists(connectorPath, function (exists){
+                    if (exists) {
+
+                        var Connector = require(connectorPath);
+
+                        _this.connect( Connector, function onConnect(err, conn){
+                            if (err) {
+                                console.error(err.stack);
+                                _this.emit('ready' ,err.stack, null);
+                            } else {
+                                //Getting Entities Manager.
+                                var entitiesManager = new require(conf.path)()[model];
+                                getModel(entitiesManager, modelPath, entitiesPath, conn)
                             }
-                            if (err) log.error('geena', 'MODEL:ERR:2', 'EEMPTY: EntitySuper' + err, __stack);
-
-                            var filename = configuration.paths.geena + '/model/entity.js';
-                            try {
-                                var ModelEntityClass = require(filename);
-                                var modelEntity = new ModelEntityClass( _this.getConfig() );
-
-                                var EntityClass = require(modelPath  + '/' + files[i]);
-                                var entity = new EntityClass();
-                                //Inherits.
-                                utils.extend(true, entity, modelEntity );
-                                //Overriding.
-                                entitiesManager[entityName] = entity;
-                                //console.log("show me ", entityName, entitiesManager[entityName],"\n\n");
-
-                            } catch (err) {
-                                console.log("Did not find entity " + modelPath + '/' + files[i] + "\nOR "+ filename);
-                            }
-
-                            //console.log("EntityManager  \n",  entitiesManager,"\n VS \n",  Entity);
-                            if (i == files.length-1) {
-                                //console.log("All done !");
-                                //var entitiesManager = new require(conf.path)()[model];
-                                _this.emit('ready', false, entitiesManager);
-                                ++i;
-                            }
-
-                        });//EO UtilsConfig.get
-
-                    }//EO produce
-
-                    var i = 0;
-                    while (i < files.length) {
-                        //console.log("TEsting entity exclusion ",  i + ": ", exluded.indexOf(files[i]) != -1 && files[i].match(/.js/), files[i]);
-                        if ( files[i].match(/.js/) && exluded.indexOf(files[i]) == -1 && !files[i].match(/.json/)) {
-                            entityName = files[i].replace(/.js/, "") + suffix;
-                            entityName = entityName.substring(0, 1).toUpperCase() + entityName.substring(1);
-                            console.log("entityName  : ", entityName );
-                            produce(entityName, i++);
-                        } else if (i == files.length-1) {
-                            //console.log("All done !");
-                            _this.emit('ready', false, entitiesManager);
-                            ++i;
-                        } else {
-                            ++i;
-                        }
-                    }//EO while.
-
-                });//EO Fs.readdir.
+                        });
+                    } else {
+                        //Means that no connector was found in models.
+                        var entitiesManager = new require(conf.path)()[model];
+                        getModel(entitiesManager, modelPath, entitiesPath)
+                    }
+                });
 
             } else {
                 _this.emit('ready', 'no configuration found for your model: ' + model);
-                console.log("no configuration found...");
+                console.log("no configuration found...")
             }
         });
 
 
     };
-    /**
-     * Get Model Configuration
-     *
-     * @return {object|undefined} configuration
-     * */
-    this.getConfig = function(){
-        if (_configuration) {
-            return _configuration;
-        } else {
-            return undefined;
+
+    this.connect = function(Connector, callback){
+        try {
+            var connector = new Connector( _this.getConfig(_connector) );
+            connector.onReady( function(err, conn){
+                callback(err, conn);
+            })
+        } catch (err){
+            console.error(err.stack);
+            callback(err)
         }
     };
 
@@ -181,20 +148,121 @@ Model = function(namespace){
 
         var configuration = config.getInstance(bundle);
         //console.log("getting for bundle ", bundle, configuration);
+        utilsConfig.get('geena', 'locals.json', function(err, locals){
+            _locals = locals;
+            if ( typeof(configuration) != 'undefined' ) {
+                var tmp = JSON.stringify(configuration);
+                tmp = JSON.parse(tmp);
+                console.log("getting for bundle ", bundle, tmp);
+                //Response.
+                var confObj = {
+                    model : tmp.content.model,
+                    path : tmp.modelsPath,
+                    locals : locals
+                };
+                callback(false, confObj);
+            } else {
+                callback('Config not instantiated');
+            }
+        });
+    };
 
-        if ( typeof(configuration) != 'undefined' ) {
-            var tmp = JSON.stringify(configuration);
-            tmp = JSON.parse(tmp);
-            console.log("getting for bundle ", bundle, tmp);
-            //Response.
-            var confObj = {
-                model : tmp.content.model,
-                path : tmp.modelsPath
-            };
-            callback(false, confObj);
+    /**
+     * Get Model Configuration
+     *
+     * @return {object|undefined} configuration
+     * */
+    this.getConfig = function(connector){
+        var connector = ( typeof(connector) == 'undefined' ) ?  _connector : connector;
+
+        if (_configuration) {
+            return ( typeof(connector) != 'undefined' ) ? _configuration[connector] : undefined;
         } else {
-            callback('Config not instantiated');
+            return undefined;
         }
+    };
+
+
+
+    /**
+     * Get model
+     *
+     * @param {object} entitiesManager
+     * @param {string} modelPath
+     * @param {string} entitiesPath
+     * @param {object} [conn]
+     * */
+    var getModel = function(entitiesManager, modelPath, entitiesPath, conn){
+        var suffix = 'Entity';
+        //var files = fs.readdirSync(entitiesPath);
+        fs.readdir(entitiesPath, function(err, files){
+
+
+            var entityName, exluded = ['index.js'];
+            var produce = function(entityName, i){
+            //console.log("producing ", files[i]);
+
+            //utilsConfig.get('geena', 'locals.json', function(err, configuration){
+            if (_locals == undefined) {
+                throw new Error("geena/utils/.gna not found.");
+            }
+            if (err) log.error('geena', 'MODEL:ERR:2', 'EEMPTY: EntitySuper' + err, __stack);
+
+            var filename = _locals.paths.geena + '/model/entity.js';
+            try {
+
+                var ModelEntityClass = require(filename);
+                var modelEntity = new ModelEntityClass( _this.getConfig(_connector) );
+                if ( typeof(conn) != 'undefined' ) {
+                    modelEntity.conn = conn;
+                }
+
+                var EntityClass = require(entitiesPath + '/' + files[i]);
+                //EntityClass.getConfig = _this.getConfig;
+
+                var entity = new EntityClass();
+                //Inherits.
+                utils.extend(true, entity, modelEntity );
+                //Overriding.
+                entitiesManager[entityName] = entity;
+                //console.log("show me ", entityName, entitiesManager[entityName],"\n\n");
+
+            } catch (err) {
+                console.warn("Did not find entity " + modelPath + '/' + files[i] + "\nOR "+ filename);
+            }
+
+            //console.log("EntityManager  \n",  entitiesManager,"\n VS \n",  Entity);
+            if (i == files.length-1) {
+                //console.log("All done !");
+                //var entitiesManager = new require(conf.path)()[model];
+                //Trigger ready event.
+                _this.emit('ready', false, entitiesManager);
+                ++i;
+            }
+
+            //});//EO UtilsConfig.get.
+
+            };//EO produce.
+
+
+            var i = 0;
+            while (i < files.length) {
+                //console.log("TEsting entity exclusion ",  i + ": ", exluded.indexOf(files[i]) != -1 && files[i].match(/.js/), files[i]);
+                if ( files[i].match(/.js/) && exluded.indexOf(files[i]) == -1 && !files[i].match(/.json/)) {
+                    entityName = files[i].replace(/.js/, "") + suffix;
+                    entityName = entityName.substring(0, 1).toUpperCase() + entityName.substring(1);
+                    console.log("entityName  : ", entityName );
+                    produce(entityName, i++);
+                } else if (i == files.length-1) {
+                    //console.log("All done !");
+                    _this.emit('ready', false, entitiesManager);
+                    ++i;
+                } else {
+                    ++i;
+                }
+            }//EO while.
+
+        });//EO Fs.readdir.
     };
 
 
@@ -214,7 +282,6 @@ Model = function(namespace){
         }
     }
 };
+
 util.inherits(Model, EventEmitter);
-
-
 module.exports = Model;
