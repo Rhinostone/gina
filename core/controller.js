e49954b2f8f529e03ec97b8cea03361ae328447b
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2014-05-02 23:21:08 +0200

    bundle add CLI done

--- core/controller.js
+++ core/controller.js
@@ -76,6 +76,7 @@ Controller = function(request, response, next) {
             _this.set('page.ext', ext);
             _this.set('page.content', content);
             _this.set('page.ext', ext);
+            _this.set('page.handler', _options.handler);
         }
     }
 
