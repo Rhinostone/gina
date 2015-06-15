8ae92260ace25b8463f935a9af1d4eb490c1ac13
Martin-Luther ETOUMAN <m.etouman@rhinostone.com>
2015-06-16 01:09:43 +0200

    added Collection type

--- core/model/entity.js
+++ core/model/entity.js
@@ -144,6 +144,31 @@ function EntitySuper(conn) {
         }
     }
 
+    ///**
+    // * Get config
+    // *
+    // * @param {string} [name] - Conf name without extension.
+    // * @return {object} result
+    // *
+    // * TODO - Protect result
+    // * */
+    //this.getConfig = function(name) {
+    //    var tmp = null;
+    //
+    //    if ( typeof(name) != 'undefined' ) {
+    //        try {
+    //            // needs to be read only
+    //            tmp = JSON.stringify(local.options.conf.content[name]);
+    //            return JSON.parse(tmp)
+    //        } catch (err) {
+    //            return undefined
+    //        }
+    //    } else {
+    //        tmp = JSON.stringify(local.options.conf);
+    //        return JSON.parse(tmp)
+    //    }
+    //}
+
     // self.trigger might be useless because of the existing self.emit(evt, err, data)
     ///**
     // * Trigger callback
