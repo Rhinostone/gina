d714b11eb201e68752300a135612866b3265a7a0
Martin-Luther <m.etouman@rhinostone.com>
2016-07-12 02:44:48 +0200

    fixed FormValidator JSON load

--- core/config.js
+++ core/config.js
@@ -1026,7 +1026,8 @@ function Config(opt) {
                             readDir( filename, forms[ k[k.length-2] ], key );
 
                         } else {
-                            key += files[i].replace('.json', '');
+                            
+                            key = files[i].replace('.json', '');
                             try {
 
                                 if (cacheless) {
