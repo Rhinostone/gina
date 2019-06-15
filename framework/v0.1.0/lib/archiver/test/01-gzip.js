0f122db1e5dcfb2beee6a52bd5b8cee211965b60
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2019-06-15 04:46:12 +0200

    improved lib/archiver to support array src input

--- framework/v0.1.0/lib/archiver/test/01-gzip.js
+++ framework/v0.1.0/lib/archiver/test/01-gzip.js
@@ -1,47 +1,27 @@
-//var reporter    = require('nodeunit').reporters.default;
+var reporter    = require('nodeunit').reporters.default;
 var fs          = require('fs');
 var archiver    = require(__dirname+'/../src/main');// Not needed if the framework installed
+var helpers     = require(__dirname+'/../../../helpers/index');// Not needed if the framework installed
 
 var target      = __dirname + '/output/';
-var filename    = __dirname + '/data/Porting to ARM 64-bit.pdf';
-var dirname     = __dirname + '/data/Samples';
+var src         = __dirname + '/data/';
+var filename    = src + 'Porting to ARM 64-bit.pdf';
+var dirname     = src + 'Samples';
+var files       = [];
+var filesList   = ['humans.txt', 'css', 'img/android-chrome-512x512.png']
+                    .forEach(function(val, key, arr){ 
+                        files[key] = {
+                            // source
+                            input   : dirname + '/' + val,
+                            // destination
+                            output  : val
+                        }
+                    });
+
 var options     = {};
 
-
-// exports['[ compress from file ] `'+ filename +'` using [ Gzip ] with default options'] = function(test) {
+exports['[ compress from file ] `'+ filename +'` using [ Gzip ] with default options'] = function(test) {
     
-//     archiver
-//         .compress(filename, target)
-//         .onComplete( function onCompressed(err, target) {
-            
-//             if (err) {
-//                 throw err;
-//             }
-            
-//             original    = fs.statSync(filename);
-//             result      = fs.statSync(target);
-//             var file    = target.substr(target.lastIndexOf('/') +1);
-            
-//             // is file
-//             test.equal(result.isFile(), true);
-//             // target crearted
-//             test.equal( fs.existsSync(target) , true);
-//             // size matches default options
-//             test.equal( original.size > result.size, true);
-            
-//             // TODO - unzip & compare original source size vs unzipped target size
-            
-//             fs.unlinkSync(target);
-
-//             test.done()
-//         })
-       
-// }
-
-
-//exports['[ compress from folder ] `'+ dirname +'` using [ Gzip ] with default options'] = function(test) {
-    /**
-    filename    = __dirname + '/data/Samples/humans.txt';    
     archiver
         .compress(filename, target)
         .onComplete( function onCompressed(err, target) {
@@ -54,52 +34,83 @@ var options     = {};
             result      = fs.statSync(target);
             var file    = target.substr(target.lastIndexOf('/') +1);
             
-            // is file
-            test.equal(result.isFile(), true);
             // target crearted
             test.equal( fs.existsSync(target) , true);
-            // size matches default options
-            test.equal( original.size > result.size, true);
-            
-            // TODO - unzip & compare original source size vs unzipped target size
-            
-            //fs.unlinkSync(target);
+            // is file
+            test.equal(result.isFile(), true);
+            // original size  vs result size using default options
+            test.equal( original.size > result.size, true);            
+                        
+            fs.unlinkSync(target);
 
             test.done()
         })
-        */
-    
+       
+}
+
+
+exports['[ compress from folder ] `'+ dirname +'` using [ Gzip ] with level 7'] = function(test) {
+        
     archiver
-        .compress(dirname, target)
+        .compress(dirname, target, { level: 7})
         .onComplete( function onCompressed(err, target) {
             
             if (err) {
                 throw err;
             }
             
-            //original    = fs.statSync(filename);
-            //result      = fs.statSync(target);
-            //var file    = target.substr(target.lastIndexOf('/') +1);
+            original    = fs.statSync(filename);
+            result      = fs.statSync(target);
             
-            // is file
-            //test.equal(result.isFile(), true);
             // target crearted
-            //test.equal( fs.existsSync(target) , true);
+            test.equal( fs.existsSync(target) , true);
+            // is file
+            test.equal(result.isFile(), true);
             // size matches default options
-            //test.equal( original.size > result.size, true);
+            test.equal( original.size > result.size, true);
             
-            // TODO - unzip & compare original source size vs unzipped target size
+            // TODO - for a later check, add a new test to unzip & check content
+            //new _(target).rmSync();
             
-            //fs.unlinkSync(target);
-
+            fs.unlinkSync(target);
             
-            //test.done()
-        });   
-        
+            test.done()
+        });  
        
-//}
+}
+
+exports['[ compress from array of filenames & dirnames ] `'+ dirname +'` using [ Gzip ] with level 7'] = function(test) {
+        
+    archiver
+        .compress(files, target, { level: 7})
+        .onComplete( function onCompressed(err, target) {
+            
+            if (err) {
+                throw err;
+            }
+            
+            original    = fs.statSync(filename);
+            result      = fs.statSync(target);
+            
+            // target crearted
+            test.equal( fs.existsSync(target) , true);
+            // is file
+            test.equal(result.isFile(), true);
+            // size matches default options
+            test.equal( original.size > result.size, true);
+            
+            // TODO - for a later check, add a new test to unzip & check content
+            //new _(target).rmSync();
+            
+            fs.unlinkSync(target);
+            
+            
+            test.done()
+        });  
+       
+}
 
 
 // for debug purpose
-// if (reporter)
-//     reporter.run(['test/01-gzip.js']);
+if (reporter)
+    reporter.run(['test/01-gzip.js']);
