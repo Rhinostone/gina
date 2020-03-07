614571e5e180f0e9f30ca6836ab4554160f17f30
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2020-03-07 04:22:03 +0100

    fixed Merge() special case

--- framework/v0.1.0/lib/merge/test/01b-merging_two_objects.js
+++ framework/v0.1.0/lib/merge/test/01b-merging_two_objects.js
@@ -2,6 +2,8 @@ var merge = require('../src/main');// Not needed if the framework installed
 
 var a = null;
 var b = null;
+var amounts = null;
+var defaultAmounts = null;
 var setVariable = function () {
 
     a = {
@@ -20,12 +22,50 @@ var setVariable = function () {
                 "file": "factsheets"
             }
         }
-    }
+    };
+    
+    amounts = {
+        "gross": 775,
+        "deposit": 0,
+        "depositValue": 0,
+        "depositType": "rate",
+        "discountValue": 0,
+        "discountType": "rate",
+        "rebateValue": 0,
+        "rebateType": "rate",
+        "net": 775,
+        "vat": [
+            {
+                "20": 155
+            }
+        ],
+        "grandTotal": 930,
+        "artistCreationValue": 0,
+        "discount": 0,
+        "rebate": 0,
+        "freelanceTotal": 930,
+        "organismTotal": 0
+    };
+    
+    defaultAmounts =  {
+        "gross": 0,
+        "deposit": 0,
+        "depositValue": 0,
+        "depositType": "rate",
+        "discountValue": 0,
+        "discountType": "rate",
+        "rebateValue": 0,
+        "rebateType": "rate",
+        "net": 0,
+        "vat": [],
+        "grandTotal": 0
+    };
+    
 };
 
 setVariable();
 var AtoBwithoutOverride = merge(a, b);
-
+var DefaultAmountsToAmountsWithoutOverride = merge(amounts, defaultAmounts);
 // setVariable();
 // var AtoBwithOverride    = merge(a, b, true);
 
@@ -42,6 +82,35 @@ exports['Merge : A<-B without override'] = function(test) {
     };
     test.equal( typeof(AtoBwithoutOverride), 'object' );
     test.deepEqual(AtoBwithoutOverride, res);
+
+    test.done()
+}
+
+exports['Merge : amounts<-defaultAmounts without override'] = function(test) {
+    var res = {
+        "gross": 775,
+        "deposit": 0,
+        "depositValue": 0,
+        "depositType": "rate",
+        "discountValue": 0,
+        "discountType": "rate",
+        "rebateValue": 0,
+        "rebateType": "rate",
+        "net": 775,
+        "vat": [
+            {
+                "20": 155
+            }
+        ],
+        "grandTotal": 930,
+        "artistCreationValue": 0,
+        "discount": 0,
+        "rebate": 0,
+        "freelanceTotal": 930,
+        "organismTotal": 0
+    };
+    test.equal( typeof(DefaultAmountsToAmountsWithoutOverride), 'object' );
+    test.deepEqual(DefaultAmountsToAmountsWithoutOverride, res);
 
     test.done()
 }
