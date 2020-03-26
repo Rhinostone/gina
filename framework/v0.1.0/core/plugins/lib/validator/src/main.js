097b0fd3323b9384b30f4e43bca08a3bdf8fb730
Martin-Luther <m.etouman@rhinostone.com>
2020-03-26 12:51:19 +0100

    fixed popin link without formValidator

--- framework/v0.1.0/core/plugins/lib/validator/src/main.js
+++ framework/v0.1.0/core/plugins/lib/validator/src/main.js
@@ -715,7 +715,7 @@ function ValidatorPlugin(rules, data, formId) {
                                     result.status = xhr.status;
                                     
                                 // if hasPopinHandler & popinIsBinded
-                                if ( typeof(gina.popin) != 'undefined' && gina.hasPopinHandler /** && gina.popinIsBinded*/ ) {
+                                if ( typeof(gina.popin) != 'undefined' && gina.hasPopinHandler ) {
                                     
                                     // select popin by id
                                     var $popin = gina.popin.getActivePopin();
