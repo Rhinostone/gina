bd5fec945eb51c0baf3a7bcc9cfa84ca255782d0
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2022-03-31 20:03:28 +0200

    Publishing npm version

--- script/prepare.js
+++ script/prepare.js
@@ -16,13 +16,46 @@ const { spawn } = require('child_process');
 var lib         = require('./lib');
 var console     = lib.logger;
 
-
+/**
+ * Pre install constructor
+ * 
+ * if you need to test, go to gina main folder
+ * $ node --inspect-brk=5858 ./script/prepare.js -g
+ *  or if you are calling `npm publish` or `npm publish --dry-run`
+ * $ node --inspect-brk=5858 /usr/local/bin/npm publish --dry-run
+ * 
+ * @constructor
+ * */
 function Prepare() {
-    var self = this;
-    this.path = getEnvVar('GINA_DIR');
-    //this.projectPath = _( __dirname.substring(0, (__dirname.length - "node_modules/gina/script".length)) );
+    var self = {};
+    self.path = getEnvVar('GINA_DIR');
+    
+    /**
+     * Bebin Checking - Will run checking tasks in order of declaration
+     * */
+     var begin = function() {
+        var i = 0;
+        var funcs = [];
+        for (let t in self) {
+            if ( typeof(self[t]) == 'function') {
+                let func = 'self.' + t + '()';
+                console.debug('Running [ ' + func + ' ]');
+                eval(func);
+                ++i
+            }
+            // end
+            if ( i == self.functionCount() ) {
+                break
+            }
+        }
+    }
 
     var init = function() {
+        self.isWin32 = isWin32();//getEnvVar('GINA_IS_WIN32');
+        self.path = getEnvVar('GINA_FRAMEWORK');
+        self.gina = getEnvVar('GINA_DIR');
+        self.root = self.gina; // by default        
+        self.isGlobalInstall = false;
         
         var args = process.argv, i = 0, len = args.length;
         for (; i < len; ++i) {
@@ -32,6 +65,8 @@ function Prepare() {
             }
         }
         
+        
+        
         if ( !self.isGlobalInstall ) { //global install
             self.root = process.cwd(); // project path
             console.error('local installation is not supported for this version at the moment.');
@@ -39,84 +74,85 @@ function Prepare() {
             process.exit(1);
         }
         
-        self.tasksCount = 0;
-        i = 0;
-        var list = [];
-        for(var t in self) {
-            if( typeof(self[t]) == 'function') {
-                list[i] = {
-                    func: self[t],
-                    cb: hasCallback(self[t])
-                }
-                ++i
-            }
-        }
-        runTask(list)
-    },
-    count = function() {
-        return self.tasksCount;
-    },
-    /**
-     * Run tasks - Will run in order of declaration
-     * */
-     runTask = function(list) {
+        begin();
+        // self.tasksCount = 0;
+        // i = 0;
+        // var list = [];
+        // for(var t in self) {
+        //     if( typeof(self[t]) == 'function') {
+        //         list[i] = {
+        //             func: self[t],
+        //             cb: hasCallback(self[t])
+        //         }
+        //         ++i
+        //     }
+        // }
+        // runTask(list)
+    }; //,
+//     count = function() {
+//         return self.tasksCount;
+//     },
+//     /**
+//      * Run tasks - Will run in order of declaration
+//      * */
+//      runTask = function(list) {
 
-        var i = self.tasksCount;
+//         var i = self.tasksCount;
 
-        if (i > list.length-1) {
-            process.exit(0)
-        }
+//         if (i > list.length-1) {
+//             process.exit(0)
+//         }
 
-        var err = false;
-        var cb = ( typeof(list[i].cb) != 'undefined' ) ? list[i].cb : false;
+//         var err = false;
+//         var cb = ( typeof(list[i].cb) != 'undefined' ) ? list[i].cb : false;
 
-        if (cb) {
-            list[i].func( function done(err, data) {
-                if (err) {
-                    console.error(err.stack||err.message);
-                    process.exit(1)
-                } else {
-                    if (data) {
-                        //var str = data.toString();
-                        console.info(data);
-                    }
-                    ++self.tasksCount;
-                    runTask(list)
-                }
+//         if (cb) {
+//             list[i].func( function done(err, data) {
+//                 if (err) {
+//                     console.error(err.stack||err.message);
+//                     process.exit(1)
+//                 } else {
+//                     if (data) {
+//                         //var str = data.toString();
+//                         console.info(data);
+//                     }
+//                     ++self.tasksCount;
+//                     runTask(list)
+//                 }
 
-            })
-        } else {
-            try {
-                list[i].func();
-                ++self.tasksCount;
-                runTask(list)
-            } catch (err) {
-                console.error(err.stack);
-                process.exit(1)
-            }
-        }
-    },
-    hasCallback = function(func) {
-        var found = false;
-        var funcString = func.toString();
-        var args = funcString.match(/^\s*function\s+(?:\w*\s*)?\((.*?)\)/);
-        args = args ? (args[1] ? args[1].trim().split(/\s*,\s*/) : []) : null;
+//             })
+//         } else {
+//             try {
+//                 list[i].func();
+//                 ++self.tasksCount;
+//                 runTask(list)
+//             } catch (err) {
+//                 console.error(err.stack);
+//                 process.exit(1)
+//             }
+//         }
+//     },
+//     hasCallback = function(func) {
+//         var found = false;
+//         var funcString = func.toString();
+//         var args = funcString.match(/^\s*function\s+(?:\w*\s*)?\((.*?)\)/);
+//         args = args ? (args[1] ? args[1].trim().split(/\s*,\s*/) : []) : null;
 
-        if (args.length > 0) {
-//            var cb;
-//            for (var a = 0; a < args.length; ++a) {
-//                console.log('......checking ', args[a]);
-//                cb = funcString.match(/(callback|cb)\s*\(/g);
-//                if (cb) {
-//                    //console.log('.......??? ', cb);
-//                    return args[a]
-//                }
-//            }
-            return args
-        }
+//         if (args.length > 0) {
+// //            var cb;
+// //            for (var a = 0; a < args.length; ++a) {
+// //                console.log('......checking ', args[a]);
+// //                cb = funcString.match(/(callback|cb)\s*\(/g);
+// //                if (cb) {
+// //                    //console.log('.......??? ', cb);
+// //                    return args[a]
+// //                }
+// //            }
+//             return args
+//         }
 
-        return found
-    };
+//         return found
+//     };
     
 
     // this.runTests = function(cb) {
@@ -136,15 +172,15 @@ function Prepare() {
     //     }
     // }
 
-    this.removeComments = function() { console.log('removin comments ...')};
-    this.removeConsoleLog = function() {
+    self.removeComments = function() { console.info('removin comments ...')};
+    self.removeConsoleLog = function() {
 
     };
     /**
      * Write version number in the VERSION file
      * */
-    this.setVersion = function() {
-        console.log('setting version number ...');
+    self.setVersion = function() {
+        console.info('setting version number ...');
         try {
             var version = require( _(self.path + 'package.json') ).version;
             var path = _(self.path + 'VERSION');
