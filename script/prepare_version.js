e6c8b02d6deffe56bdba5d8ac23b6eef41a82ed8
Martin-Luther <m.etouman@rhinostone.com>
2022-07-03 03:50:54 +0200

    Prerelease update - 2022-07-03T03:50:54

--- script/prepare_version.js
+++ script/prepare_version.js
@@ -394,7 +394,7 @@ function PrepareVersion() {
     }
 
     self.tagVersionIfNeeded = function(done) {
-        console.info('Is on Dry Run', process.argv);
+        console.error('Is on Dry Run', process.argv);
         process.stdout.write('Is on Dry Run', process.argv);
         if ( typeof(self.isOnDryRun) != 'undefined' ) {
 
