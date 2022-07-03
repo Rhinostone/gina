1c92265b79391b7a0a2ff8442d1098c84c1b90bc
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2022-07-03 03:39:00 +0200

    Prerelease update - 2022-07-03T03:39:00

--- script/prepare_version.js
+++ script/prepare_version.js
@@ -42,7 +42,6 @@ function PrepareVersion() {
     var self    = {};
 
     var init = function() {
-        console.log('Is on Dry Run', process.argv);
         self.isWin32 = isWin32();
         var args = process.argv, i = 0, len = args.length;
         for (; i < len; ++i) {
@@ -395,9 +394,8 @@ function PrepareVersion() {
     }
 
     self.tagVersionIfNeeded = function(done) {
-
         if ( typeof(self.isOnDryRun) != 'undefined' ) {
-
+            console.debug('Is on Dry Run', process.argv);
             done()
         }
 
