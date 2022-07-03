39d8a9ab1ed74188798634b5b7939f475d095f06
Martin-Luther <m.etouman@rhinostone.com>
2022-07-03 02:56:24 +0200

    Prerelease update

--- script/prepare_version.js
+++ script/prepare_version.js
@@ -23,36 +23,36 @@ var helpers     = null;
 var lib         = null;
 /**
  * PrepareVersion constructor
- * 
- * NB.: 
+ *
+ * NB.:
  *  This script can only be executed on Mac OS X or on Linux distributions
- *  It will: 
+ *  It will:
  *      - commit & push all modifications to the appropriate branch
  *      - create if needed a new branch for the new version & remotely push to this new branch
  *      - update local default version with the new one
- * 
+ *
  * if you need to test, go to gina main folder
  * $ node --inspect-brk=5858 ./script/prepare_version.js -g
  *  or if you are calling `npm publish` or `npm publish --dry-run`
  * $ node --inspect-brk=5858 /usr/local/bin/npm publish --dry-run
- * 
+ *
  * @constructor
  * */
 function PrepareVersion() {
     var self    = {};
-    
+
     var init = function() {
         self.isWin32 = isWin32();
         begin(0);
     };
-    
+
     /**
      * Bebin - Will run checking tasks in order of declaration
      * */
      var begin = async function(i) {
         //console.debug('i is ', i);
         var n = 0, funct = null, functName = null;
-        for (let t in self) {            
+        for (let t in self) {
             if ( typeof(self[t]) == 'function') {
                 if (n == i){
                     //let func = 'self.' + t + '()';
@@ -61,33 +61,33 @@ function PrepareVersion() {
                     funct       = func;
                     functName   = t;
                     break;
-                }                
+                }
                 n++;
             }
         }
-        
+
         // to handle sync vs async to allow execution in order of declaration
         if (funct) {
-            eval('async function on'+functName+'(){ await promisify('+ funct + ')().catch(function(e){ console.error(e); process.exit(-1);}).then(function(){ begin('+(i+1)+')});}; on'+functName+'();'); // jshint ignore:line
+            eval('async function on'+functName+'(){ await promisify('+ funct + ')().catch(function(e){ console.error(e.toString()); process.exit(-1);}).then(function(){ begin('+(i+1)+')});}; on'+functName+'();'); // jshint ignore:line
         } else {
             process.exit(0);
-        } 
+        }
     }
-       
-    
+
+
     self.getSelectedVersion = async function(done) {
         var homeDir = getUserHome() || null;
-        
-        if (!homeDir) {        
+
+        if (!homeDir) {
             return done(new Error('No $HOME path found !'))
         }
-        
+
         var ginaHomeDir = homeDir.replace(/\n/g, '') + '/.gina';
         setEnvVar('GINA_HOMEDIR', ginaHomeDir);
-        
+
         console.debug('GINA_HOMEDIR: ', ginaHomeDir);
         console.debug('isWin32: ', isWin32());
-        
+
         var mainConfigPath = ginaHomeDir +'/main.json';
         var mainConfig = require(mainConfigPath);
         var package = require(pack);
@@ -95,10 +95,10 @@ function PrepareVersion() {
         self.selectedVersion = selectedVersion;
         var targetedVersion = package.version.replace(/^v/, '');
         self.targetedVersion = targetedVersion;
-        
+
         console.debug('selected version : ', selectedVersion);
         console.debug('targeted version : ', targetedVersion);
-        
+
         // setting up requirements
         var shortVersion = selectedVersion.split('.');
         shortVersion.splice(2);
@@ -107,9 +107,9 @@ function PrepareVersion() {
         var settingsConfig      = require(settingsConfigPath);
         var ginaPath            = settingsConfig.dir;
         self.ginaPath = ginaPath;
-            
+
         var frameworkPath       = ginaPath +'/framework/v'+selectedVersion;
-        self.frameworkPath      = frameworkPath;        
+        self.frameworkPath      = frameworkPath;
         helpers     = require(frameworkPath +'/helpers');
         lib         = require(frameworkPath +'/lib');
 
@@ -120,84 +120,84 @@ function PrepareVersion() {
         if ( typeof(mainConfig.frameworks[shortVersion]) == 'undefined' ) {
             mainConfig.frameworks[shortVersion] = [];
             // create settings.json for the new version
-            
+
         }
         if ( mainConfig.frameworks[shortVersion].indexOf(targetedVersion) < 0 ) {
             mainConfig.frameworks[shortVersion].push(targetedVersion)
         }
-        
-        
+
+
         settingsConfigPath  = ginaHomeDir+'/'+shortVersion+'/settings.json';
         settingsConfig      = require(settingsConfigPath);
-        
+
         // setting def_framework
-        mainConfig.def_framework    = targetedVersion;            
-        settingsConfig.version      = targetedVersion;            
-        ginaPath                    = settingsConfig.dir;            
+        mainConfig.def_framework    = targetedVersion;
+        settingsConfig.version      = targetedVersion;
+        ginaPath                    = settingsConfig.dir;
         self.ginaPath = ginaPath;
         // backup of folder version to archives
-        
-        
-        
+
+
+
         // console.debug('mainConfig: ', JSON.stringify(mainConfig, null, 2));
         // console.debug('settingsConfig: ', JSON.stringify(settingsConfig, null, 2));
-        
+
         // saving local config
         lib.generator.createFileFromDataSync(JSON.stringify(mainConfig, null, 2), mainConfigPath);
         lib.generator.createFileFromDataSync(JSON.stringify(settingsConfig, null, 2), settingsConfigPath);
-        
-        
+
+
         var frameworkPathObj    =  new _(frameworkPath, true);
         console.debug('source path is: '+ frameworkPath);
-        
+
         var destination = _(ginaHomeDir +'/archives/framework/v'+selectedVersion, true);
         if ( new _(destination).existsSync() ) {
             new _(destination).rmSync();
         }
         var err = false;
-        
+
         // since we cannot yet promissify directly PathObject.cp()
         var f = function(destination, cb) {
             frameworkPathObj.cp(destination, cb);
-        };            
-        await promisify(f)(destination)  
+        };
+        await promisify(f)(destination)
             .catch( function onCopyError(_err) {
                 err = _err;
             })
             .then( function onCopy(_destination) {
                 console.debug('Copy to '+ _destination +': done');
             });
-        
+
         if (err) {
             throw err;
         }
-        
+
         // rename folder version
         destination = _(ginaPath +'/framework/v'+targetedVersion, true);
         frameworkPathObj.renameSync(destination);
-        
-        
+
+
         // updating requirements
         self.selectedVersion = targetedVersion;
         self.frameworkPath = frameworkPath = ginaPath +'/framework/v'+targetedVersion;
         helpers             = require(frameworkPath +'/helpers');
         lib                 = require(frameworkPath +'/lib');
-        
+
         done()
     };
-    
+
     self.setupScriptCWD = function(done) {
         var currentWorkingDir = process.cwd();
         if ( self.ginaPath != currentWorkingDir ) {
             console.debug('Changing current working dir from `'+ currentWorkingDir +'` to `'+ self.ginaPath +'`');
-            process.chdir(self.ginaPath);    
+            process.chdir(self.ginaPath);
         }
-        
+
         done();
     }
-    
+
     self.updateVersionIfNeeded = function(done) {
-        
+
         var version = self.selectedVersion.replace(/^[a-z]+/ig, '');
         var versionFilePath = _(self.frameworkPath +'/VERSION', true);
         var versionFilePathObj = new _(versionFilePath);
@@ -210,15 +210,15 @@ function PrepareVersion() {
             }
             versionFilePathObj.rmSync()
         }
-        
-        console.debug('updating version ...');              
+
+        console.debug('updating version ...');
         lib.generator.createFileFromDataSync(version, versionFilePath);
-        
+
         done();
     }
-    
+
     self.updateMiddlewareIfNeeded = function(done) {
-        
+
         var version = self.selectedVersion.replace(/^[a-z]+/ig, '');
         var middleware = 'isaac@'+version; // by default
         var deps = require(_(self.frameworkPath, true) + '/package.json').dependecies;
@@ -234,7 +234,7 @@ function PrepareVersion() {
         } else if (typeof(middleware) == 'undefined') {
             throw new Error('No middleware found !!');
         }
-        
+
         var middlewareFilePath = _(self.frameworkPath +'/MIDDLEWARE', true);
         var middlewareFilePathObj = new _(middlewareFilePath);
         if ( middlewareFilePathObj.existsSync() ) {
@@ -246,17 +246,17 @@ function PrepareVersion() {
             }
             middlewareFilePathObj.rmSync()
         }
-        
-        console.debug('updating middleware ...');        
+
+        console.debug('updating middleware ...');
         lib.generator.createFileFromDataSync(middleware, middlewareFilePath);
-        
+
         done();
     };
-    
+
     self.removeAllSymlinks = function(done) {
-        
+
         // remove existing symlinks
-        var versionsFolders = null, frameworkPath = null;        
+        var versionsFolders = null, frameworkPath = null;
         try {
             frameworkPath = _(self.ginaPath +'/framework', true);
             console.info('frameworkPath: ', frameworkPath);
@@ -267,14 +267,14 @@ function PrepareVersion() {
         } catch (err) {
             // do nothing
         }
-        
+
         for (let i = 0, len = versionsFolders.length; i < len; i++) {
             let dir = versionsFolders[i];
             // skip junk
             if ( /^\./i.test(dir) || /(\s+copy|\.old)$/i.test(dir) ) {
                 continue;
             }
-            
+
             // intercept & remove symlinks
             try {
                 if ( fs.lstatSync( _(frameworkPath +'/'+ dir, true) ).isSymbolicLink() ) {
@@ -284,16 +284,16 @@ function PrepareVersion() {
                 continue;
             }
         }
-        
+
         done();
     }
-    
-    
+
+
     self.pushChangesToGit = function(done) {
-        
+
         var cmd = null;
         var version = self.selectedVersion.replace(/^[a-z]+/ig, '');
-        
+
         // getting current branch
         // git rev-parse --abbrev-ref HEAD
         // => 010
@@ -306,27 +306,27 @@ function PrepareVersion() {
             // nothing to do
             return done( new Error('[GIT] No branch selected') )
         }
-                
-        
+
+
         // create new branch if needed
         // e.g: 0.1.0-alpha.1 -> 010-alpha1
         var targetedBranch = version.replace(/\./g, '');
-        
+
         console.debug('[GIT] Current branch: '+ currentBranch);
         console.debug('[GIT] Targeted branch: '+ targetedBranch);
-        
-        
+
+
         // check if targeted branch exists
         // git rev-parse --verify 011
         var branchExists = null;
         try {
             branchExists = execSync("git rev-parse --verify "+ targetedBranch)
                             .toString()
-                            .replace(/(\n|\r|\t)/g, '');                
+                            .replace(/(\n|\r|\t)/g, '');
         } catch (err) {
-            // nothing to do                
+            // nothing to do
         }
-        
+
         if (!branchExists) {
             console.debug('No existing branch found, creating a new one !');
             try {
@@ -336,55 +336,56 @@ function PrepareVersion() {
                 execSync("git push --set-upstream origin "+ targetedBranch);
             } catch (err) {
                 console.error(err.stack||err.message||err);
-                return done(err);              
+                return done(err);
             }
         }
         // use existing to push updates
         else {
-            
+
             if (currentBranch != targetedBranch) {
                 console.debug('Switching from branch `'+ currentBranch +'` to branch `'+ targetedBranch +'`');
                 // git checkout 010
                 try {
-                    cmd = execSync("git checkout "+ targetedBranch);                
+                    cmd = execSync("git checkout "+ targetedBranch);
                 } catch (err) {
                     console.error(err.stack||err.message||err);
-                    return done(err);               
+                    return done(err);
                 }
             } else {
                 console.debug('Reusing branch `'+ targetedBranch +'`');
             }
         }
-        
-        
-        // git add --all        
+
+
+        // git add --all
         try {
-            cmd = execSync("git add --all ");                
+            cmd = execSync("git add --all ");
         } catch (err) {
             console.error(err.stack||err.message||err);
-            return done(err);               
+            return done(err);
         }
-        // git commit -m'Packaging version v'+ version            
+        // git commit -m'Packaging version v'+ version
         try {
             var msg = (!branchExists) ? 'New version' : 'Prerelease update';
-            cmd = execSync("git commit -am'"+ msg +"'");                
+            cmd = execSync("git commit -am'"+ msg +"'");
         } catch (err) {
+
             console.error(err.stack||err.message||err);
-            return done(err);               
+            return done(err);
         }
-        
+
         console.debug('Pushing changes made on branch `'+ targetedBranch +'` to git `origin/'+ targetedBranch +'`');
         // git push origin 010
         try {
-            cmd = execSync("git push origin "+ targetedBranch );                
+            cmd = execSync("git push origin "+ targetedBranch );
         } catch (err) {
             console.error(err.stack||err.message||err);
-            return done(err);               
+            return done(err);
         }
-        
+
         done()
     }
-    
+
     init()
 }
 
