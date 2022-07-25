4c34b181bb0f2085a4214e9f452cc06bbbe88789
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2022-07-25 04:09:45 +0200

    New version

---
+++ framework/v0.1.1-alpha.110/lib/cmd/framework/restart.js
@@ -0,0 +1,152 @@
+var fs          = require('fs');
+const {spawn}       = require('child_process');
+const {execSync}    = require('child_process');
+const util = require('util');
+
+var CmdHelper   = require('./../helper');
+// const { start } = require('repl');
+var console     = lib.logger;
+/**
+ * Framework restart
+ *
+ * e.g.
+ *  gina framework:restart
+ *  or
+ *  gina restart
+ *  or
+ *  gina restart @1.0.0
+ *
+ * */
+function Restart(opt, cmd) {
+    var self    = {
+        // Current version of the framework by default
+        // But can be overriden with argument: @{version_number}
+        // eg.: gina stop @1.0.0
+        version: GINA_VERSION
+    };
+
+
+    var init = function(opt, cmd) {
+        // import CMD helpers
+        new CmdHelper(self, opt.client, { port: opt.debugPort, brkEnabled: opt.debugBrkEnabled });
+        var err = null;
+        // check CMD configuration
+        //if (!isCmdConfigured()) return false;
+        // checkcking version number
+        if ( typeof(opt.argv[3]) != 'undefined' && /^@/.test(opt.argv[3]) ) {
+            var version = opt.argv[3].replace(/\@/, '');
+            var shortVersion = version.split('.').splice(0,2).join('.');
+            if ( !/^\d\.\d/.test(shortVersion) ) {
+                err = new Error('Wrong version: '+ version);
+                console.log(err.message);
+                return;
+            }
+            var availableVersions = requireJSON(_(GINA_HOMEDIR +'/main.json', true)).frameworks[shortVersion];
+            if ( availableVersions.indexOf(version) < 0 ) {
+                err = new Error('Version not installed: '+ version);
+                console.log(err.message);
+                return;
+            }
+
+            self.version = version;
+        }
+        console.debug('Restarting framework v'+ self.version);
+
+        // if (!self.name) {
+        //     stop(opt, cmd, 0);
+        // } else {
+            restart(opt, cmd);
+        // }
+    }
+
+    var restart = function(opt, cmd) {
+        stop();
+        start();
+    }
+
+    var stop = function() {
+        var out = null;
+        try {
+            out = execSync('gina stop @'+self.version).toString();
+            console.debug('out => ', out);
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    /**
+     * start
+     *
+     * We need to spawn this one as detached
+     * because of the `process.kill(..., 'SIGSTOP')` used inside the orginal `start` script
+     * or else, the restart script will be pending forever.
+     */
+    var start = async function() {
+        console.debug('continue with start now');
+        try {
+            // var child = spawn('gina', ['start', '@'+self.version],
+            var child = spawn('gina', ['start', '@'+self.version, '--restart-pid='+ process.pid],
+                {
+                    detached: true
+                }
+            );
+
+            var frameworkPid = null;
+            child.stdout.setEncoding('utf8');
+            child.stdout.on('data', function(data) {
+                //process.stdout.write(data);
+                if ( new RegExp('Gina server started with PID','gi').test(data) ) {
+                    restartRunningBunldes();
+                    frameworkPid = data.match(/\`\d+\`/)[0];
+                    process.stdout.write('Gina server started with PID '+ frameworkPid + '\r\n');
+                }
+
+                if ( /\[ quit \]/.test(data) ) {
+                    // TODO - restart all running bundles
+                    end()
+                }
+            });
+
+            child.stderr.setEncoding('utf8');
+            var error = null;
+            child.stderr.on('data', function(err) {
+                error = err.toString();
+                console.error(error);
+            });
+
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    var restartRunningBunldes = function() {
+        var list = fs.readdirSync(_(GINA_RUNDIR, true));
+        for (let i=0, len=list.length; i<len; i++ ) {
+            let file = list[i];
+            if (/^\./.test(file) || !/\.pid$/.test(file) || /^gina\-/.test(file) || /(minion)/.test(file) ) {
+                continue;
+            }
+            // process.stdout.write('\ngina bundle:restart '+ file.replace(/\.pid$/, '').replace(/\@/, ' @') + '\n');
+            execSync('gina bundle:restart '+ file.replace(/\.pid$/, '').replace(/\@/, ' @'));
+        }
+    }
+
+
+    var end = function (err, type, messageOnly) {
+        if ( typeof(err) != 'undefined') {
+            var out = ( typeof(messageOnly) != 'undefined' && /^true$/i.test(messageOnly) ) ? err.message : (err.stack||err.message);
+            if ( typeof(type) != 'undefined' ) {
+                console[type](out)
+            } else {
+                console.error(out);
+            }
+        }
+
+        process.exit( err ? 1:0 )
+    }
+
+
+    init(opt, cmd)
+}
+
+module.exports = Restart;
---
+++ framework/v0.1.1-alpha.110/lib/cmd/framework/status.js
@@ -0,0 +1,121 @@
+var fs          = require('fs');
+const {spawn}       = require('child_process');
+const {execSync}    = require('child_process');
+//const { debug } = require('console');
+
+var CmdHelper   = require('./../helper');
+var console     = lib.logger;
+/**
+ * Framework status
+ *
+ * e.g.
+ *  gina framework:status
+ *  or
+ *  gina status
+ *
+ * */
+function Status(opt, cmd) {
+    var self    = {};
+
+
+    var init = function(opt, cmd) {
+
+        console.debug('Getting framework status');
+
+        // import CMD helpers
+        new CmdHelper(self, opt.client, { port: opt.debugPort, brkEnabled: opt.debugBrkEnabled });
+
+        status(opt, cmd);
+    }
+
+    var checkUnregistered = function(pidFiles) {
+        // Those not in file
+        var list = execSync("ps -ef | grep -v grep | grep 'gina-v' | awk '{print $2\" \"$8\" \"$9}'").toString().replace(/\n$/, '').split(/\n/g);
+
+
+        if (!list.length || list[0] == "") {
+            return;
+        }
+        // console.debug('pids list ', list);
+        for (let p=0, len=list.length; p<len; p++) {
+            if ( !/^\d+\s+gina\-/.test(list[p]) ) {
+                continue;
+            }
+
+            let pidArr = list[p].split(/\s/);
+            let pid = pidArr[0];
+            let title = pidArr[1];
+            let isZombie = ( typeof(pidArr[2]) != 'undefined' && /defunct/.test(pidArr[2]) ) ? true : false;
+
+            // remove defunct process
+            if (isZombie) {
+                execSync("kill -9 "+ pid);
+                continue;
+            }
+
+            let file = title +'.pid';
+            if ( pidFiles.indexOf( file ) < 0) {
+                fs.writeFileSync( _(GINA_RUNDIR +'/'+ file, true), pid );
+                pidFiles.push(title +'.pid');
+            }
+        }
+    }
+
+    var status = function(opt, cmd) {
+        var pidFiles = null;
+        try {
+            pidFiles = fs.readdirSync(GINA_RUNDIR);
+        } catch (fileError) {
+            throw fileError
+        }
+        checkUnregistered(pidFiles);
+        console.debug('pidFiles ',pidFiles);
+
+        var runningVersions = [], runningLog = '';
+        for (let i=0, len=pidFiles.length; i<len; i++) {
+            let file = pidFiles[i];
+            if ( !/^gina\-/.test(file) ) {
+                continue;
+            }
+            let pid = fs.readFileSync(_(GINA_RUNDIR +'/'+ file)).toString().trim() || null;
+            runningVersions.push({
+                title   : file.replace(/\.pid$/, ''),
+                pid     : ~~pid
+            });
+
+            let version = file.replace(/^gina\-/, '').replace(/\.pid$/, '');
+            runningLog +=  'Running: '+ version;
+            if (version == 'v'+GINA_VERSION ) {
+                runningLog += ' (default)'
+            }
+            runningLog += '\n';
+        }
+
+
+        if ( runningVersions.length > 0 ) {
+            console.log(runningLog);
+            return end()
+        }
+
+        console.log('Gina is not running');
+        end();
+    }
+
+    var end = function (err, type, messageOnly) {
+        if ( typeof(err) != 'undefined') {
+            var out = ( typeof(messageOnly) != 'undefined' && /^true$/i.test(messageOnly) ) ? err.message : (err.stack||err.message);
+            if ( typeof(type) != 'undefined' ) {
+                console[type](out)
+            } else {
+                console.error(out);
+            }
+        }
+
+        process.exit( err ? 1:0 )
+    }
+
+
+    init(opt, cmd)
+}
+
+module.exports = Status;
