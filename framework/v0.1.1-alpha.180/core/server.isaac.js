74bbc73902ca47bf6ad21b8c2fd29651b1aa14d8
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2023-02-28 11:36:44 +0100

    Prerelease update

---
+++ framework/v0.1.1-alpha.180/core/server.isaac.js
@@ -0,0 +1,391 @@
+//"use strict";
+/**
+ * Isaac Server Integration
+ *
+ */
+const fs                    = require('fs');
+const {EventEmitter}        = require('events');
+const Eio                   = require('engine.io');
+
+const lib               = require('./../lib');
+const inherits          = lib.inherits;
+const merge             = lib.merge;
+const console           = lib.logger;
+
+const env       = process.env.NODE_ENV
+    , isDev     = (/^true$/i.test(process.env.NODE_ENV_IS_DEV)) ? true : false
+;
+
+var refreshCore = function() {
+
+    var corePath    = getPath('gina').core;
+    var libPath     = getPath('gina').lib;
+
+    var core        = new RegExp( corePath );
+    var excluded    = [
+        _(corePath + '/gna.js', true)
+    ];
+
+    for (let c in require.cache) {
+        if ( (core).test(c) && excluded.indexOf(c) < 0 ) {
+            require.cache[c].exports = require( _(c, true) )
+        }
+    }
+
+    // Update lib & helpers
+    delete require.cache[require.resolve(_(libPath +'/index.js', true))];
+    require.cache[_(libPath +'/index.js', true)] = require( _(libPath +'/index.js', true) );
+    require.cache[_(corePath + '/gna.js', true)].exports.lib = require.cache[_(libPath +'/index.js', true)];
+
+    // Update plugins
+    delete require.cache[require.resolve(_(corePath +'/plugins/index.js', true))];
+    require.cache[_(corePath +'/plugins/index.js', true)] = require( _(corePath +'/plugins/index.js', true) );
+    require.cache[_(corePath + '/gna.js', true)].exports.plugins = require.cache[_(corePath +'/plugins/index.js', true)];
+}
+
+// Express compatibility
+const slice = Array.prototype.slice;
+
+
+function ServerEngineClass(options) {
+
+    console.debug('[ ENGINE ] Isaac says hello !');
+
+    // TOTO - See if it would be interesting to add it to Helper::Path & to extend it to also readdirSync, returning the directory content
+    var readSync = function(filename) {
+        var fileObj = new _(filename, true);
+        if ( fileObj.isSymlinkSync() ) {
+            filename = fileObj.getSymlinkSourceSync()
+        }
+
+        return fs.readFileSync(filename).toString()
+    }
+
+    // openssl req -x509 -newkey rsa:2048 -nodes -sha256 -subj "/CN=localhost" -keyout localhost-privkey.pem -out localhost-cert.pem
+    var credentials = {};
+    if ( /https/.test(options.scheme) ) {
+        try {
+            credentials = {
+                key: readSync(options.credentials.privateKey),
+                cert: readSync(options.credentials.certificate)
+            };
+        } catch(err) {
+            console.emerg('You are trying to start a secured server (https) wihtout suficient credentials: check your `server settings`\n'+ err.stack);
+            process.exit(1)
+        }
+    }
+
+
+    var allowHTTP1 = true; // by default
+    if (typeof (options.allowHTTP1) != 'undefined' && options.allowHTTP1 != '' ) {
+        allowHTTP1 = options.allowHTTP1;
+    }
+    credentials.allowHTTP1 = allowHTTP1;
+
+
+    if (typeof (options.credentials.ca) != 'undefined' && options.credentials.ca != '' )
+        credentials.ca = options.credentials.ca;
+
+    if (typeof (options.credentials.pfx) != 'undefined' && options.credentials.pfx != '' )
+        credentials.pfx = readSync(options.credentials.pfx);
+
+    if (typeof (options.credentials.passphrase) != 'undefined' && options.credentials.passphrase != '' )
+        credentials.passphrase = options.credentials.passphrase;
+
+    var server = null, http = null, ioServer = null;
+
+    if ( /^http\/2/.test(options.protocol) ) {
+        var http2   = require('http2');
+        switch (options.scheme) {
+            case 'http':
+                server      = http2.createServer({ allowHTTP1: allowHTTP1 });
+                break;
+
+            case 'https':
+                server      = http2.createSecureServer(credentials);
+                break;
+
+            default:
+                server      = http2.createServer({ allowHTTP1: allowHTTP1 });
+                break;
+        }
+    } else {
+
+        switch (options.scheme) {
+            case 'http':
+                http        = require('http');
+                server      = http.createServer();
+                break;
+
+            case 'https':
+                var https   = require('https');
+                server      = https.createServer(credentials);
+                break;
+
+            default:
+                http        = require('http');
+                server      = http.createServer();
+                break;
+        }
+    }
+
+
+    const middleware = function(path, cb) {
+
+        // if (request.path === path) {
+        //     onPath.call(this, path, cb)
+        // }  else { // 404
+        //     stream.respond({
+        //         'content-type': 'text/html',
+        //         ':status': 404
+        //     });
+        //     stream.end('<h1>404</h1>');
+        // }
+    }
+
+
+    const onPath = function(path, cb, allowAll) {
+
+        var queryParams = null
+            , i         = null
+            , len       = null
+            , p         = null
+            , arr       = null
+            , a         = null
+        ;
+
+        // http2stream handle by the Router class & the SuperController class
+        // See `${core}/router.js` & `${core}/controller/controller.js`
+
+        server.on('request', (request, response) => {
+
+            if (isDev) {
+                refreshCore()
+            }
+
+            if ( /engine.io/.test(request.url)) {
+                console.debug('io request');
+            }
+
+            if (/^\*$/.test(path) || path == request.url) {
+                request.params  = {};
+                request.query   = {};
+
+                if ( /\?/.test(request.url) ) {
+
+                    queryParams = request.url.split(/\?/);
+
+                    len = queryParams.length;
+                    request.params[0] = queryParams[0];
+
+                    if ( /\&/.test(queryParams[1]) ) {
+                        i = 1;
+                        for (; i < len; ++i) {
+
+                            arr = queryParams[i].split(/\&/);
+                            p = 0;
+                            for (; p < arr.length; ++p) {
+                                a = arr[p].split(/\=/);
+                                // false & true case
+                                if ( /^(false|true|on)$/i.test(a[1]) )
+                                    a[1] = ( /^(true|on)$/i.test(a[1]) ) ? true : false;
+                                else if (a[1] && a[1].indexOf('%') > -1)
+                                    a[1] = decodeURIComponent(a[1]);
+
+                                if (/^(\{|\[)/.test(a[1]) ) {
+                                    try {
+                                        a[1] = JSON.parse(a[1]);
+                                    } catch(notAJsonError) {
+                                        console.warn('[SERVER][INCOMING REQUEST]', 'Could not convert to JSON or Array this key/value to :' + a[0] + ': '+a[1] +'/nLeaving value as a string.');
+                                    }
+                                }
+                                request.query[ a[0] ] = a[1]
+                            }
+                        }
+                    } else {
+                        a = queryParams[1].split(/\=/);
+
+                        if (a.length > 1) {
+                            // false & true case
+                            if ( /^(false|true|on)$/i.test(a[1]) )
+                                a[1] = ( /^(true|on)$/i.test(a[1]) ) ? true : false;
+
+                            request.query[ a[0] ] = a[1]
+                        } else { // for redirection purposes or when passing `?encodedJsonObject`
+                            try {
+                                if ( a[0].indexOf('%') > -1 ) { // encoded URI Component
+                                    a[0] = decodeURIComponent(a[0])
+                                }
+
+                                request.query = a[0] ? JSON.parse(a[0]) : {};
+                            } catch(err) {
+                                console.error(err.stack)
+                            }
+                        }
+
+                    }
+                    request.url = request.url.replace(/\?.*/, '')
+                } else {
+                    request.params[0] = request.url
+                }
+
+
+                cb(request, response);
+            }
+        });
+
+    }
+
+
+    // All paths allowed
+    server.all = function(path, cb) {
+        onPath.call(this, path, cb, true)
+    }
+
+    // configuring express plugins|middlewares
+    server._expressMiddlewares = [];
+    /**
+     * <server>.use()
+     * Applies middleware for every single route
+     *
+     * @param {function} fn
+     */
+    server.use = function use(fn) {
+
+        var offset = 0;
+        //var path = '/';
+
+        // default path to '/'
+        // disambiguate app.use([fn])
+        if (typeof fn !== 'function') {
+          var arg = fn;
+
+          while (Array.isArray(arg) && arg.length !== 0) {
+            arg = arg[0];
+          }
+
+          // first arg is the path
+          if (typeof arg !== 'function') {
+            offset = 1;
+            path = fn;
+          }
+        }
+
+
+        var fns = merge(slice.call(arguments, offset));
+
+        if (fns.length === 0) {
+          throw new TypeError('server.use() requires a middleware function')
+        }
+
+        fns.forEach(function (fn) {
+            server._expressMiddlewares[server._expressMiddlewares.length] = fn;
+        });
+
+        return this;
+    }
+
+
+    server.on('error', (err) => {
+        console.error(err)
+    });
+
+
+    //------------------------------------
+    // Engine IO server
+    //------------------------------------
+    if (
+        typeof(options.ioServer) != 'undefined'
+        && typeof(options.ioServer.integrationMode) != 'undefined'
+        && /^attach$/.test(options.ioServer.integrationMode)
+    ) {
+        console.info('[IO SERVER ] `eio` found using `'+ options.ioServer.integrationMode +'` integration mode');
+        delete options.ioServer.integrationMode;
+        // test done in case we would like to switch to socket.io-server
+        ioServer = ( typeof(Eio.attach) != 'undefined' ) ? new Eio.attach(server, options.ioServer) : new Eio(server, options.ioServer);
+
+        ioServer.getClientsBySessionId = function(sessionId) {
+
+            if (this.clients.count() == 0)
+                return null;
+
+            var clients = null;
+
+            for (var id in this.clients) {
+
+                if ( typeof(this.clients[id].sessionId) == 'undefined' )
+                    continue;
+
+                if (!clients) clients = {};
+
+                clients[id] = this.clients[id];
+            }
+
+            if (clients) {
+                // allowing to send for each client found sharing the same request session
+                clients.send = function(payload, option, callback) {
+                    for ( var id in this) {
+                        if ( typeof(this[id].sessionId) == 'undefined')
+                            continue;
+
+                        this[id].send(payload, option, callback)
+                    }
+                }
+            }
+
+
+            return clients;
+        }
+
+        server.eio = ioServer;
+
+        ioServer.on('connection', function (socket) {
+
+            socket.send(JSON.stringify({
+                id: this.id,//socket.id,
+                handshake: 'Welcomed to `'+ options.bundle +'` main socket !',
+                // how many ms before sending a new ping packet
+                pingTimeout: options.ioServer.pingTimeout || options.ioServer.timeout,
+                // how many ms without a pong packet to consider the connection closed
+                pingInterval: options.ioServer.pingInterval || options.ioServer.interval
+            }));
+
+            socket.on('message', function(payload){
+
+                try {
+                    console.debug('[IO SERVER ] receiving '+ payload);
+                    payload = JSON.parse(payload);
+                    // bind to session ID
+                    if ( typeof(payload.session) != 'undefined' ) {
+                        this.sessionId = payload.session.id;
+                    }
+                } catch(err) {
+                    console.error(err.stack||err.message|| err)
+                }
+            });
+
+            socket.on('close', function(){
+                console.debug('[IO SERVER ] closed socket #'+ this.id);
+            });
+        });
+
+        server.on('upgrade', function(req, socket, head){
+            console.debug('[IO SERVER ] upgrading socket #'+ this.id);
+            ioServer.handleUpgrade(req, socket, head);
+        });
+        // httpServer.on('request', function(req, res){
+        //     ioServer.handleRequest(req, res);
+        // });
+
+
+    }
+
+
+
+    return {
+        instance: server,
+        middleware: middleware
+    }
+};
+
+module.exports = ServerEngineClass;
---
+++ framework/v0.1.1-alpha.180/lib/logger/src/containers/file/index.js
@@ -0,0 +1,346 @@
+'use strict';
+// Imports
+const fs                  = require('fs');
+const util                = require('util');
+//const {EventEmitter}             = require('events');
+const net                 = require('net');
+// var promisify           = util.promisify;
+// const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+// const { execSync }      = require('child_process');
+
+var helpers = null;
+
+var merge     = require(__dirname + '/../../../../merge');
+if ( typeof(merge) != 'function' ) {
+    // if needed for unit tests
+    merge = eval(fs.readFileSync(__dirname +'/../../../../merge/src/main.js').toString())
+}
+
+function FileContainer(opt, loggers) {
+    var self = {
+        // flow or container name/id
+        name: 'file'
+    };
+
+    var mqId = 'MQ'+ self.name.substring(0,1).toLocaleUpperCase() + self.name.substring(1);
+
+    helpers         = require(__dirname +'/../../../../../helpers');
+
+
+    var loggerHelper    = require(__dirname +'/../../helper.js')(opt, loggers);
+    var format          = loggerHelper.format;
+    var processProperties = null;
+    var filenames   = {};
+
+    function init(opt) {
+
+        // ---------- BO - hack for early calls
+        var isWin32         = (process.platform === 'win32') ? true : false;
+        var binPath         = __dirname +'/../../../../../../../';
+        var ginaPath        = (binPath.replace(/\\/g, '/')).replace('/bin', '');
+        ginaPath = (isWin32) ? ginaPath.replace(/\//g, '\\') : ginaPath;
+        // loading pack
+        var pack            = ginaPath + '/package.json';
+        pack = (isWin32) ? pack.replace(/\//g, '\\') : pack;
+        var packObj         = require(pack);
+        var version         = packObj.version;// jshint ignore:line
+        // var frameworkPath   = ginaPath + '/framework/v' + version;
+
+
+        var shortVersion = version.split('.');
+        shortVersion.splice(2);
+        shortVersion = shortVersion.join('.');
+
+        var settings = { mq_port: 8125, host_v4: '127.0.0.1' };
+        try {
+            settings = require( getUserHome() + '/.gina/' + shortVersion + '/settings.json');
+        } catch (err) {}
+
+        opt.mqPort = settings.mq_port;
+        opt.hostV4 = settings.host_v4;
+        // ---------- EO - hack for early calls
+
+        // handle server not started yet or server exited
+        // process.on('gina#mqlistener-started', function onGinaStarted(mqPort, hostV4, group) {
+        //     if (group) {
+        //         console.info('[MQFile] Group `'+group+'` connected `'+ hostV4 +'` on port `'+ mqPort +'` :)');
+        //     }
+        //     clearInterval(nIntervId);
+        //     nIntervId = null;
+        //     onPayload({mqPort: mqPort, hostV4: hostV4});
+        // });
+
+        process.on('gina#bundle-logging', function onBundleStarted(mqPort, hostV4, group) {
+            console.debug('[MQFile] resuming ...')
+            if (group) {
+                console.info('[MQFile] Group `'+group+'` connected `'+ hostV4 +'` on port `'+ mqPort +'` :)');
+            }
+            // only if tail not already running !! Or you will get duplicate logs
+            onPayload({mqPort: mqPort, hostV4: hostV4}, true);
+        });
+
+        onPayload(opt);
+
+        // ----------------------------Debug---------------------------------------
+        var level = 'debug';
+        // Init debugging - Logs not in hierarchy will just be ignored
+        if (opt.hierarchies[opt.hierarchy].indexOf( opt.levels[level].code) > -1) {
+            process.emit('logger#'+self.name, JSON.stringify({
+                group       : opt.name,
+                level       : level,
+                // Raw content !
+                content     : '`'+ self.name +'` logger container loaded !'
+            }));
+        }
+        // ------------------------------------------------------------------------
+    }
+
+    function setup(group, filenames, props) {
+        //var group = process.title; // gina, frontend@myproject ...
+        console.log('['+ mqId +'] setting up '+ group);
+
+        // we only want the bundle's logs
+        if (
+            !/\@/.test(group)
+            // ||
+            // props.bundles
+            // && props.bundles.length == 0
+            // ||
+            // props.bundles
+            // && props.bundles.indexOf(group) < 0
+        ) {
+            return
+        }
+
+        if ( !filenames[group] ) {
+            filenames[group] = {}
+        }
+        process.stdout.write( format(opt.name, 'info', '['+ mqId +'] setting up '+ group +'\nFilenames: '+ JSON.stringify(filenames, null, 2)) );
+        /// aready defiened
+        if ( filenames[group].filename) {
+            return
+        }
+
+        // retriving hostname
+        var bfnArr = group.split(/\@/);
+        var bundleName = bfnArr[0];
+        var projectName = bfnArr[1];
+        var homeDir = getUserHome() || process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];// jshint ignore:line
+        homeDir += '/.gina';
+        var project = requireJSON(_(homeDir +'/projects.json', true))[projectName];// jshint ignore:line
+        var projectPath = project.path;
+        // console.debug('logger::file write env: ', bundleName, process.env.NODE_ENV, getEnvVar('GINA_ENV'), JSON.stringify(process.gina, null, 2) );// jshint ignore:line
+
+        var env         = process.env.NODE_ENV || getEnvVar('GINA_ENV');// jshint ignore:line
+        var Config      = require(getEnvVar('GINA_CORE') + '/config');
+        var conf = new Config({
+            env: env,
+            projectName: projectName,
+            executionPath: projectPath,
+            startingApp: bundleName,
+            ginaPath: getEnvVar('GINA_CORE')
+        }, true).getInstance(bundleName);
+
+        var envObj      = conf.envConf[bundleName][env];// jshint ignore:line
+        var webroot     = ( !/\s+|\//.test(envObj.server && envObj.server.webroot) ) ? envObj.server.webroot +'.' : '';
+        var hostname    = envObj.host;
+        var logDir      = getLogDir() || getEnvVar('GINA_LOGDIR');// jshint ignore:line
+
+        filenames[group].filename = _(logDir +'/'+ webroot + hostname +'.log', true);// jshint ignore:line
+        console.debug('Log group `'+ group +'` filename set to: ' + filenames[group].filename);
+        process.stdout.write( format(opt.name, 'info', 'Log group `'+ group +'` filename set to: ' + filenames[group].filename) );
+    }
+
+    function onPayload(opt, isResuming) {
+        // console.debug('mqFile options: ', JSON.stringify(opt, null, 2));
+        var port = opt.mqPort || getEnvVar('GINA_MQ_PORT') || 8125;// jshint ignore:line
+        var host = opt.hostV4 || getEnvVar('GINA_HOST_V4') || '127.0.0.1';// jshint ignore:line
+        var clientOptions = {
+            host    : host,
+            port    : port,
+            request : 'writeToFile'
+        }
+        // var loggerOptions   = console.getOptions();
+        // var loggers         = console.getLoggers();
+        // var loggerHelper    = LoggerHelper(loggerOptions, loggers);
+        // var format          = loggerHelper.format;
+
+
+
+
+        var delayedMessages = [];
+        var resume = function(payload) {
+            // process.stdout.write('['+ mqId +'] Resuming with group: '+ payload.group);
+            var i = 0;
+            while (i < delayedMessages.length) {
+                let pl = delayedMessages[i];
+                // debug only
+                // process.stdout.write('['+ mqId +']'+ format(pl.group, pl.level, pl.content) );
+
+                write(pl.group, format(pl.group, pl.level, pl.content) );
+                i++;
+            }
+            delayedMessages = []
+        }
+
+
+
+
+        var client = net.createConnection(clientOptions, () => {
+            // 'connect' listener.
+            console.info('['+ mqId +'] connected to server :) on host: '+ host + ' & port: '+ port, process.argv);
+            processProperties = loggerHelper.getProcessProperties();
+            console.info('['+ mqId +'] process properties ', processProperties);
+
+            process.emit('gina#container-writting', host, port);
+
+            // send request
+            client.write( JSON.stringify(clientOptions) +'\r\n');
+
+        });
+
+
+        client.on('error', (data) => {
+            var err = data.toString();
+            process.stdout.write( format(opt.name, 'warn', '['+ mqId +'] ' + err) );
+
+            // allowing the framework to quit properly
+            if ( /write EPIPE|read ECONNRESET|connect ECONNREFUSED/i.test(err) ) {
+                process.exit(0)
+            }
+
+            // var mqPort = null;
+            // nIntervId = setInterval(() => {
+            //     try {
+            //         mqPort = ~~(fs.readFileSync(mqPortFile).toString());
+            //         if (mqPort) {
+            //             process.emit('gina#mqlistener-started', mqPort, host);
+            //         }
+            //     } catch (fileErr) {}
+            // }, 100);
+        });
+
+        var payloads = null, i = null;
+        client.on('data', (data) => {
+
+            //console.log('['+ mqId +']  (data): ' + data.toString());
+            payloads = data.toString();
+
+            // from speakers
+            if ( /^(\{\"|\[\{\")/.test(payloads) ) {
+                payloads = payloads.split(/\r\n/g);
+                i = -1;
+                while(i < payloads.length) {
+                    i++;
+                    let payload = payloads[i];
+                    if (
+                        /^\{/.test(payload) && /\}$/.test(payload)
+                        || /^\[\{/.test(payload) && /\}\]$/.test(payload)
+                    ) {
+                        let pl = null;
+                        try {
+                            pl = JSON.parse(payload);
+                        } catch(plErr) {
+                            process.stdout.write( '['+ mqId +'] (exception) '+ payload +'\n' );
+                            continue;
+                        }
+
+
+                        if (!pl.content) {
+                            // only for debug
+                            // process.stdout.write( '['+ mqId +'] (undefined content) '+ JSON.stringify(pl, null) +'\n' );
+
+                            // updating logger context since it can run on different processes
+                            if ( pl.sessionId && !clientOptions.sessionId ) {
+                                clientOptions.sessionId = pl.sessionId;
+                                // setting up file descriptor if not existing
+
+                                //setup(pl.group, processProperties);
+                                for ( let b = 0, bLen = processProperties.bundles.length; b < bLen; b++) {
+                                    let realGroup = processProperties.bundles[b];
+                                    setup(realGroup, filenames, processProperties);
+                                }
+
+                                // acknowledging
+                                client.write( JSON.stringify(clientOptions) +'\r\n');
+                            }
+
+                            if (pl.loggers) {
+                                loggers = merge(loggers, pl.loggers);
+                                if (delayedMessages.length > 0 /**&& /\@/.test(pl.group)*/ ) {
+                                    resume(pl)
+                                }
+                            }
+                            continue;
+                        }
+
+                        // resuming logging from another process
+                        // we do not want to print twice in this case since another logger server is already running
+                        if (isResuming) {
+                            return
+                        }
+
+                        // only for debug
+                        // process.stdout.write(  '['+ mqId +'] '+ pl.content +'\n' );
+
+                        try {
+                            if (!filenames[pl.group].filename ) {
+                                return;
+                            }
+                            write(pl.group, format(pl.group, pl.level, pl.content) );
+
+                        } catch (writeErr) {
+                            // means that the related MQSpeaker is not connected yet
+                            // this can happen during `bundle:start` configuration
+                            // we'll then delay the output until MQSpeaker is ready
+                            delayedMessages.push(pl);
+                        }
+
+                    }
+                }
+            }
+
+        });
+        client.on('end', () => {
+            console.log('['+ mqId +'] disconnected from server');
+        });
+        // setInterval(() => {}, 1 << 30);
+    }
+
+    function write(group, content) {
+
+        if ( !/\@/.test(group) || !filenames[group] ) {
+            process.stdout.write( '['+ mqId +']['+ group +'] '+ content );
+            return;
+        }
+
+        if ( !filenames[group].filename ) {
+            //throw new Error('['+ mqId +'] No filename found!');
+            process.emit('logger#'+self.name, JSON.stringify({
+                group       : group,
+                level       : 'error',
+                // Raw content !
+                content     : new Error('['+ mqId +'] No filename found!')
+            }));
+            return;
+        }
+        var data = new Uint8Array(Buffer.from(content));
+        fs.writeFile(filenames[group].filename, data,
+            {
+                encoding: "utf8",
+                // https://nodejs.org/api/fs.html#file-system-flags
+                flag: "a"
+            },
+            (err) => {
+                if (err) {
+                    process.stdout.write( format(opt.name, 'err', '['+ mqId +'] ' + err) );
+                    return;
+                }
+        });
+    }
+
+
+
+    init(opt);
+}
+module.exports = FileContainer;
