1bf89049c48ce150a8449feb2911f47057a750ee
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2023-03-17 00:57:53 +0100

    Fixed install issues

--- script/post_publish.js
+++ script/post_publish.js
@@ -7,7 +7,6 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
-
 //Imports.
 var fs          = require('fs');
 var util        = require('util');
