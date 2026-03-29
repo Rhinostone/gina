5c7eccd3bde4e5c36905041ef2552823ca283a8e
Martin Luther ETOUMAN NDAMBWE <martinluther@gina.io>
2026-03-29 17:49:52 +0100

    Release v0.2.0

--- framework/v0.2.0/core/deps/swig-1.4.2/docs/js/swig.js
+++ framework/v0.2.0/core/deps/swig-1.4.2/docs/js/swig.js
@@ -1,1 +1,1 @@
-/Users/martinlutheretouman/.npm-global/lib/node_modules/gina/framework/v0.1.9-alpha.2/core/asset/plugin/../../deps/swig-1.4.2/bin/swig.js
+/Users/martinlutheretouman/.npm-global/lib/node_modules/gina/framework/v0.2.0/core/asset/plugin/../../deps/swig-1.4.2/bin/swig.js
