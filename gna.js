cec1a91d0c9ddd59f5663d0ea5704b81e1a2137f
Martin Luther ETOUMAN <m.etouman@rhinostone.com>
2026-05-14 19:09:42 +0100

    Release v0.3.13

--- gna.js
+++ gna.js
@@ -15,14 +15,14 @@
 'use strict';
 
 // Framework core — the main gna module (lifecycle hooks, lib, etc.)
-var _gna = require('./framework/v0.3.13-alpha.4/core/gna');
+var _gna = require('./framework/v0.3.13/core/gna');
 
 // SuperController and EntitySuper — loaded from their source modules
-var SuperController = require('./framework/v0.3.13-alpha.4/core/controller');
-var EntitySuper     = require('./framework/v0.3.13-alpha.4/core/model/entity');
+var SuperController = require('./framework/v0.3.13/core/controller');
+var EntitySuper     = require('./framework/v0.3.13/core/model/entity');
 
 // uuid — from the lib registry
-var uuid = require('./framework/v0.3.13-alpha.4/lib/uuid');
+var uuid = require('./framework/v0.3.13/lib/uuid');
 
 module.exports = {
 
