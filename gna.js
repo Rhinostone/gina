1122cb67c9c67a0806e3a25108e1abb24f2c935e
Martin Luther ETOUMAN NDAMBWE <m.etouman@rhinostone.com>
2026-08-25 13:55:34 +0100

    Version bump to 0.6.17-alpha.1

--- gna.js
+++ gna.js
@@ -15,14 +15,14 @@
 'use strict';
 
 // Framework core — the main gna module (lifecycle hooks, lib, etc.)
-var _gna = require('./framework/v0.6.16/core/gna');
+var _gna = require('./framework/v0.6.17-alpha.1/core/gna');
 
 // SuperController and EntitySuper — loaded from their source modules
-var SuperController = require('./framework/v0.6.16/core/controller');
-var EntitySuper     = require('./framework/v0.6.16/core/model/entity');
+var SuperController = require('./framework/v0.6.17-alpha.1/core/controller');
+var EntitySuper     = require('./framework/v0.6.17-alpha.1/core/model/entity');
 
 // uuid — from the lib registry
-var uuid = require('./framework/v0.6.16/lib/uuid');
+var uuid = require('./framework/v0.6.17-alpha.1/lib/uuid');
 
 module.exports = {
 
